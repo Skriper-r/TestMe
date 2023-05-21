c0f54056072f9e035c8459af6fa4d439014bd2cc
av_rod <scriper-r@gmail.com>
2023-05-21 18:52:28 +0300

    test add person and find person

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -7,7 +9,12 @@ faker_ru = Faker('ru_RU')
 def generated_person():
     yield Person(
         full_name=faker_ru.name(),
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
         email=faker_ru.email(),
+        age=random.randint(10, 80),
+        salary=random.randint(1000, 10000),
+        department=faker_ru.job(),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address()
     )
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,4 +1,7 @@
-from pages.element_page import TextBoxPage, CheckBoxPage, RadioButtonPage
+import random
+import time
+
+from pages.element_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
 
 
 class TestElements:
@@ -36,6 +39,26 @@ class TestElements:
             output_impressive = radio_button_page.get_output_result()
             radio_button_page.click_on_the_radio_button('no')
             output_no = radio_button_page.get_output_result()
-            assert output_yes  == 'Yes', '"Yes" have not been selected'
+            assert output_yes == 'Yes', '"Yes" have not been selected'
             assert output_impressive == 'Impressive', '"Impressive" have not been selected'
             assert output_no == 'None', '"No" have not been selected'
+
+    class TestWebTable:
+        def test_seb_table_add_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            new_person = web_table_page.add_new_person()
+            table_result = web_table_page.check_new_added_person()
+            assert new_person in table_result
+
+
+        def test_web_table_search_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            key_word = web_table_page.add_new_person()[random.randint(0, 5)]
+            web_table_page.search_some_person(key_word)
+            time.sleep(5)
+            table_result = web_table_page.check_new_added_person()
+            print(key_word)
+            print(table_result)
+            assert key_word in table_result, 'The person was not found in the table'
